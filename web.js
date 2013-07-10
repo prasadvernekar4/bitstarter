55631febadb52a5e659f1ce35c732173ce16ff1a
Prasad Vernekar <prasadvernekar4@gmail.com>
2013-07-10 23:15:25 +0000

    Edited file

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('I am in IBMer! \n just another brick in the wall \n \t \t- Pink Floyd');
+  response.send('Another brick in the wall \n \t \t- Pink Floyd');
 });
 
 var port = process.env.PORT || 5000;
