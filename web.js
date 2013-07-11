18fe63ae95879b4db1b15df0f356b10aa441ccca
Prasad Vernekar <prasadvernekar4@gmail.com>
2013-07-11 13:13:30 +0000

    Updated web.js_html

--- web.js
+++ web.js
@@ -1,12 +1,14 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ //response.send(buffer.toString('utf8',0,50));
+var k = fs.readFileSync('./index.html','utf8');
+ response.send( k );
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+ console.log("Listening on " + port);
 });
